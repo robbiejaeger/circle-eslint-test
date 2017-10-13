c307ab78028303a22b2bf49d63b05cd36bf0a4b1
Robbie Jaeger <jaeger.rob@gmail.com>
2017-10-12 20:46:49 -0600

    Add basic server file with endpoint

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+
+app.locals.breads = [
+  {
+    id: 1,
+    type: "sourdough"
+  },
+  {
+    id: 2,
+    type: "rye"
+  }
+];
+
+app.get('/api/v1/breads', (request, response) => {
+  response.status(200).json(app.locals.breads);
+});
+
+app.listen(app.get('port'), () => {
+  console.log('App running on port ' + app.get('port'));
+});
